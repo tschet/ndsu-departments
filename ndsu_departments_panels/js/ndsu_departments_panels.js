37ca2369a1c401b57d3a0d6ebe5483664cd02ce8
Douglas Tschetter <me@douglast.com>
2015-09-01 14:41:01 -0500

    updated Views

--- ndsu_departments_panels/js/ndsu_departments_panels.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-
-jQuery(document).ready(function($) {
-   $('body.college-department-term #primary-nav ul.menu li a[href="/college-department"]').parent('li').addClass('active-trail');
-})
-// Replacing active-trail class on terms that may hae been removed by panels term-view
